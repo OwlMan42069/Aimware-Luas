f996af1b462df8456e59fa262f5b6b39c4619a6d
OwlMan42069 <84178057+OwlMan42069@users.noreply.github.com>
2021-05-20 04:49:59 -0400

    Update and rename Hentai Killsay Deathsay Remastered.lua to Hentai Killsay Deathsay.lua

--- Hentai Killsay Deathsay.lua
+++ Hentai Killsay Deathsay.lua
@@ -1,5 +1,5 @@
 local SCRIPT_FILE_NAME = GetScriptName();
-local SCRIPT_FILE_ADDR = "https://raw.githubusercontent.com/OwlMan42069/Aimware-Luas/main/Hentai%20Killsay%20Deathsay%20Remastered.lua";
+local SCRIPT_FILE_ADDR = "https://raw.githubusercontent.com/OwlMan42069/Aimware-Luas/main/Hentai%20Killsay%20Deathsay.lua";
 local VERSION_FILE_ADDR = "https://raw.githubusercontent.com/OwlMan42069/Aimware-Luas/main/Versions/Hentai%20Killsay%20Deathsay%20Version.txt";
 local VERSION_NUMBER = "1.1";
 local version_check_done = false;
