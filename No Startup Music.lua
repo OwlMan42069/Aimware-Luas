d17e66b6b21f37c9c94db6d7d9c0c0559c990b57
OwlMan42069 <84178057+OwlMan42069@users.noreply.github.com>
2021-06-22 00:40:50 -0400

    Update No Startup Music.lua
    The command now only runs once (on launch) instead of at all times.

--- No Startup Music.lua
+++ No Startup Music.lua
@@ -1,4 +1,2 @@
 -- No More Menu Music for new accounts by RetardAlert! Make sure to set the lua as autorun.
-callbacks.Register('Draw', function()
-    client.Command("snd_menumusic_volume 0", true);
-end)
+client.Command("snd_menumusic_volume 0", true);
