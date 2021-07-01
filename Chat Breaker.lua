b0bd6d48fb50cf4288cd65f6c4afe0a5ce82d006
OwlMan42069 <84178057+OwlMan42069@users.noreply.github.com>
2021-07-01 05:03:10 -0400

    Add files via upload

---
+++ Chat Breaker.lua
@@ -0,0 +1,17 @@
+-- Chat Breaker by RetardAlert (https://aimware.net/forum/user/419881)
+local ref = gui.Reference("Misc" , "Enhancement", "Appearance")
+local chat_breaker = gui.Checkbox(ref, "chat.breaker", "Chat Breaker", false)
+chat_breaker:SetDescription("Clears chat when a player sends a message.")
+
+local function ChatBreaker( msg )    
+    if msg:GetID() == 6 and chat_breaker:GetValue() == true then
+        local index = msg:GetInt( 1 );
+        local message = msg:GetString( 4, 1 );
+        local name = client.GetPlayerNameByIndex( index );
+        local breaker = {
+        "﷽﷽ ﷽﷽﷽ ﷽﷽﷽ ﷽﷽﷽ ﷽﷽﷽ ﷽﷽﷽ ﷽﷽﷽ ﷽﷽﷽ ﷽﷽﷽﷽ ﷽﷽﷽ ﷽﷽﷽ ﷽﷽﷽ ﷽﷽",
+        };
+        client.ChatSay( breaker[math.random(#breaker)] )
+      end
+  end
+callbacks.Register( "DispatchUserMessage", "UserMessage", ChatBreaker );
