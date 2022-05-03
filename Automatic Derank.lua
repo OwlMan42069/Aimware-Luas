b194c2676d945358fe0764ed35ab109f79be7e59
OwlMan42069 <84178057+OwlMan42069@users.noreply.github.com>
2022-05-02 23:20:58 -0400

    Add files via upload

---
+++ Automatic Derank.lua
@@ -0,0 +1,14 @@
+local ref = gui.Reference('Misc', 'General', 'Extra')
+local AutoDerank = gui.Checkbox(ref, 'auto.derank', 'Automatic Derank', false)
+AutoDerank:SetDescription('Have the rest of your team leave before the match starts.')
+
+local function Derank(e)
+    if e:GetName() == 'round_prestart' or e:GetName() == 'round_start' and AutoDerank:GetValue() then
+        client.Command('disconnect', true)
+        panorama.RunScript("CompetitiveMatchAPI.ActionReconnectToOngoingMatch()")
+    end
+end
+
+client.AllowListener('round_prestart')
+client.AllowListener('round_start')
+callbacks.Register('FireGameEvent', Derank)
